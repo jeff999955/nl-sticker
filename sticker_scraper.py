31a3d51794603f4ddcacf01715bd4d54f2313879
Jeff <jeff999955@gmail.com>
2023-09-03 11:00:15 +0800

    Use general scraper, add cli arg parsing

---
+++ sticker_scraper.py
@@ -0,0 +1,65 @@
+import argparse
+import json
+import os
+import shutil
+import tempfile
+
+import requests
+import tqdm
+
+
+def main(args):
+    with open(args.json_file, "r") as f:
+        sticker_list = json.load(f)
+
+    for sticker in tqdm.tqdm(sticker_list):
+        src, name = sticker["src"], sticker["name"]
+
+        resolution = src.split("/")[-1]
+        src = src.replace(resolution, "4.0")
+        img = requests.get(src).content
+
+        # Check the file type with the first 4 bytes
+        signature = img[:4]
+
+        if signature == b"\x89PNG":
+            extension = "png"
+        elif signature == b"GIF8":
+            extension = "gif"
+        else:
+            print(f"Unknown image signature for {name}.")
+            continue
+
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            f.write(img)  # Write the image to the file
+
+        shutil.move(
+            f.name, os.path.join(args.output_dir, f"{name}.{extension}")
+        )  # Move the file to the correct path
+
+
+def parse_cli_args():
+    # Parse command line arguments with argparse
+    parser = argparse.ArgumentParser(description="Download stickers with json list.")
+    parser.add_argument(
+        "-f",
+        "--file",
+        "--json-file",
+        help="Path to the json file containing the list of stickers.",
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        "--output-dir",
+        help="Path to the output directory.",
+        required=True,
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_cli_args()
+    main(args)
