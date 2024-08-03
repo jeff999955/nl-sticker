c63401bad3d587c6ae8daf17ac56b761ba2c2fca
Jeff <jeff999955@gmail.com>
2024-08-02 17:10:10 -0700

    use logger

--- src/download.py
+++ src/download.py
@@ -1,14 +1,19 @@
-import json
+import logging
 import requests
 from parser.twitch import parse_json
 from scraper.twitch import get_access_token, get_emotes
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 def get_twitch_emotes():
     try:
         access_token = get_access_token()
     except requests.exceptions.HTTPError as e:
-        print(f"Failed to get access token: {e}")
+        logger.error(f"Failed to get access token: {e}")
         return
     except KeyError as e:
         return
@@ -16,24 +21,26 @@ def get_twitch_emotes():
     try:
         emotes = get_emotes(access_token)
     except requests.exceptions.HTTPError as e:
-        print(f"Failed to get emotes: {e}")
+        logger.error(f"Failed to get emotes: {e}")
         return
 
     try:
         emotes = parse_json(emotes)
     except KeyError as e:
-        print(f"Failed to parse emotes: {e}")
+        logger.error(f"Failed to parse emotes: {e}")
         return
 
     for emote in emotes:
         url = emote["url"]
         response = requests.get(url)
         if response.status_code != 200:
-            print(f"Failed to download emote: {emote['name']}")
+            logger.error(f"Failed to download emote: {emote['name']}")
             continue
 
         with open(f"emotes/{emote['name']}.{emote['extension']}", "wb") as f:
             f.write(response.content)
+
+        logger.info(f"Downloaded emote: {emote['name']}")
 
 
 def main():
